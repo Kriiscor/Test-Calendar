98fa9add04cd8396122dbf568bf7c436332d1364
Corentin Dubail <Corentindubail@gmail.com>
2020-10-01 15:39:33 +0200

    yes

--- indexzz.php
+++ indexzz.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Calendar</title>
+</head>
+<body>
+    <h1> Site publier sur Github </h1>
+</body>
+</html>
 <?php
 /** 
  * Script de contr�le et d'affichage du cas d'utilisation "Rechercher"
